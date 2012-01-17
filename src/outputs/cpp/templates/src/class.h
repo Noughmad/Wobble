f7137abf0d95dcfb897f8088b202e29e1064a31c
Miha Čančula <miha@noughmad.eu>
2012-01-17 21:12:47 +0100

    Improve the C++ class header template

--- src/outputs/cpp/templates/src/class.h
+++ src/outputs/cpp/templates/src/class.h
@@ -4,12 +4,20 @@
 namespace {{ nameSpace }} {
 {% endif %}
 
-class {{ class.name }} {% if class.superclasses %}:{% endif %} {% for super in class.superclasses %} super{% if not forloop.last %}, {% endif %}{% endfor %}
+class {{ name }}Private;
+
+class {{ name }} {% if class.superclasses %}:{% endif %} {% for super in class.superclasses %} super{% if not forloop.last %}, {% endif %}{% endfor %}
 {
     Q_OBJECT
     {% for p in class.properties %}
     Q_PROPERTY({{ p.type }} {{ p.name }} {% if p.getter %}READ {{ p.getter }} {% endif %}{% if p.setter %}WRITE {{ p.setter }} {% endif %})
     {% endfor %}
+    
+public:
+    {{ class.name }}(QObject* parent = 0);
+    
+protected:
+    Q_DECLARE_PRIVATE({{ class.name }})
 };
 {% if nameSpace %}
 }
