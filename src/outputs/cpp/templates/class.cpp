ee3cac4ff833ca8b1bfdb99975b2cc94e2f80bfe
Miha Čančula <miha@noughmad.eu>
2012-01-20 14:02:32 +0100

    Make C++ output useful without D-pointers

--- src/outputs/cpp/templates/class.cpp
+++ src/outputs/cpp/templates/class.cpp
@@ -9,38 +9,43 @@
 {% if nameSpace %}using namespace nameSpace;{% endif %}
 
 {% if dpointer %}
+
 {{ name }}Private::{{ name }}Private()
-{
-    
-}
+{% templatetag openbrace %}
+{% templatetag closebrace %}
 
 {{ name }}Private::~{{ name }}Private()
-{
-    
-}
+{% templatetag openbrace %} 
+{% templatetag closebrace %}
 
-{% endif dpointer %}
+{% endif %}
 
 {{ name }}::{{ name }}(QObject* parent = 0) : QObject(parent)
-{
+{% templatetag openbrace %}
     
-}
+{% templatetag closebrace %}
 
 virtual ~{{ name }}()
-{
+{% templatetag openbrace %}
     
-}
+{% templatetag closebrace %}
 
 {% for p in properties %}
 {{ p.type|return_arg }} {{ name }}::{{ p|getter_name }}()
-{
-    Q_D(const {{ name }});
-    return d->{{ name }};
-}
+{% templatetag openbrace %}    
+    {% if dpointer %}
+    Q_D(const {{ p.name }});
+    return d->{{ p.name }};
+    {% endif %}
+    return m_{{ p.name }};
+{% templatetag closebrace %}
 
-void {{ name }}::{{ p|setter_name }}({{ p.type|pass_arg }} {{name}})
-{
-    Q_D({{ name }});
-    d->{{ name }} = {{ name }};
-}
+void {{ name }}::{{ p|setter_name }}({{ p.type|pass_arg }} {{ p.name }})
+{% templatetag openbrace %}    
+    {% if dpointer %}
+    Q_D({{ p.name }});
+    d->{{ p.name }} = {{ p.name }};
+    {% endif %}
+    m_{{ p.name }} = {{ p.name }};
+{% templatetag closebrace %}
 {% endfor %}
